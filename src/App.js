219c8194137577b5560a7c0bf2c9239374dff5a7
alserned <alserned@gmail.com>
2021-07-01 21:44:25 -0700

    dev docker permissions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        I changed now
+        Fuck fuck fuck
         </p>
         <a
           className="App-link"
